b0da0f4cf57142d423f53fc930c07fce2635c2b1
Mauro Arteaga <mauroprogrammer7@gmail.com>
2024-09-09 17:15:57 -0400

    feat: New Proyect

---
+++ src/.internal/createMathOperation.ts
@@ -0,0 +1,32 @@
+import baseToNumber from './baseToNumber.js'
+import baseToString from './baseToString.js'
+
+/**
+ * Creates a function that performs a mathematical operation on two values.
+ *
+ * @private
+ * @param {Function} operator The function to perform the operation.
+ * @param {number} [defaultValue] The value used for `undefined` arguments.
+ * @returns {Function} Returns the new mathematical operation function.
+ */
+function createMathOperation(operator, defaultValue) {
+  return (value, other) => {
+    if (value === undefined && other === undefined) {
+      return defaultValue
+    }
+    if(value === undefined || other === undefined) {
+      return value || other
+    }
+    if (typeof value === 'string' || typeof other === 'string') {
+      value = baseToString(value)
+      other = baseToString(other)
+    }
+    else {
+      value = baseToNumber(value)
+      other = baseToNumber(other)
+    }
+    return operator(value, other)
+  }
+}
+
+export default createMathOperation
